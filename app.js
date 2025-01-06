0eb2fe1ff166270fe4f9e89c51fa46df903218ef
BauriSandip <baurisandip117@gmal.com>
2025-01-06 17:37:49 +0530

    pubg

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,13 +8,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/crud-api', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
+mongoose.connect("mongodb://localhost/crud-api", {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
 });
 
 // Define a mongoose model and schema
-const Todo = mongoose.model('Todo', {
+const Todo = mongoose.model("Todo", {
   title: String,
   description: String,
 });
@@ -22,46 +22,49 @@ const Todo = mongoose.model('Todo', {
 // Routes go here
 
 //implementation crud application
-app.post('/todos', async (req, res) => {
-    try {
-      const todo = new Todo(req.body);
-      await todo.save();
-      res.status(201).send(todo);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  });
+app.post("/todos", async (req, res) => {
+  try {
+    const todo = new Todo(req.body);
+    await todo.save();
+    res.status(201).send(todo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
-  app.get('/todos', async (req, res) => {
-    try {
-      const todos = await Todo.find();
-      res.send(todos);
-    } catch (error) {
-      res.status(500).send(error);
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.send(todos);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!todo) {
+      return res.status(404).send();
     }
-  });
-  app.put('/todos/:id', async (req, res) => {
-    try {
-      const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
-      if (!todo) {
-        return res.status(404).send();
-      }
-      res.send(todo);
-    } catch (error) {
-      res.status(400).send(error);
+    res.send(todo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) {
+      return res.status(404).send();
     }
-  });
-  app.delete('/todos/:id', async (req, res) => {
-    try {
-      const todo = await Todo.findByIdAndDelete(req.params.id);
-      if (!todo) {
-        return res.status(404).send();
-      }
-      res.send(todo);
-    } catch (error) {
-      res.status(500).send(error);
-    }
-  });
+    res.send(todo);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+console.log("hello pubg");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
